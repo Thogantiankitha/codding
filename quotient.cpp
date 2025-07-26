a00cd3be8ca4b6aaf4f67be03f70b584429a03a9
Thogantiankitha <thogantiankitha917@gmail.com>
2025-07-26 18:10:11 +0530

    Create quotient.cpp

---
+++ quotient.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+using namespace std;
+int main(){
+    int a,b,quotient;
+    cout<<"enter the value of a:";
+    cin>>a;
+    cout<<"enter the value of b:";
+    cin>>b;
+     quotient=a/b;
+    cout<<"quotient="<< quotient<<endl;
+}
