32206464c12756c10c58a7b8ce20532b943b75a8
Thogantiankitha <thogantiankitha917@gmail.com>
2025-07-26 18:02:07 +0530

    Create product.cpp

---
+++ product.cpp
@@ -0,0 +1,12 @@
+ #include<iostream>
+using namespace std;
+int main(){
+    int a,b,product;
+    cout<<"enter the value of a:";
+    cin>>a;
+    cout<<"enter the value of b:";
+    cin>>b;
+    product=a*b;
+    cout<<"product="<<product<<endl;
+}
+
